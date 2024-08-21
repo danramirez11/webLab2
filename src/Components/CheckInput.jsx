a7c12c60d5a45fc99e8a33775cda2bd5c8a1a037
Dan <daramirezp1112@gmail.com>
2024-08-21 15:07:33 -0500

    feat: input check

--- src/Components/CheckInput.jsx
+++ src/Components/CheckInput.jsx
@@ -1,7 +1,17 @@
+const checkStyle = {
+    backgroundColor: 'white',
+    color: 'black',
+    padding: '20px',
+    borderRadius: '20px',
+    border: 'none',
+    'box-shadow': '0 0 4px rgba(0, 0, 0, 0.3)',
+    cursor: 'pointer'
+}
+
 const CheckInput = (props) => {
     return (
-        <button>
-            {props.answer}
+        <button style={checkStyle}>
+            Hola hola
         </button>
     )
 }
--- src/screens/Quizz.jsx
+++ src/screens/Quizz.jsx
@@ -3,6 +3,7 @@ import CheckInput from "../Components/checkInput";
 const Quizz = () => {
     return (
         <>
+        <h1>Which pet is the best for you?</h1>
         <CheckInput></CheckInput>
         </>
     )
